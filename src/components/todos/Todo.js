631f90b3ec0a1f657a4a39885a9dbe87bd80deb0
codewithirene567 <irenejinscott@gmail.com>
2020-09-21 16:28:11 -0400

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,19 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
-
+const Todo = props => {
+    //debugger
+    //props.todo.text is coming from mapStateToProps where it is returning the props as attributes of a tod
+    //in this case the only attribute a todo has besides an id is text
+return (
+<div>
+    <span>{props.todo.text}</span><button onClick={() =>
+   props.delete(props.todo.id)}>DELETE</button>
+</div>
+//providing function able to be deleting things
+//now we are modifying the todo component to use props.todo.id on click
+//but only providing id as payload to delete things
+)
+}
 export default Todo;
+
+// <li>{props.text}</li>
--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -4,8 +4,13 @@ import Todo from './Todo'
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
-
+  renderTodos = () => {
+    return this.props.todos.map(todo => <Todo delete={this.props.delete} key={todo.id} todo={todo} />)
+  }
+  //updating our todos container
+  //renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+//this.props.todos is coming from our action at the bottom, gives access for each todo component to 
+//access our delete todo's action
   render() {
     return(
       <div>
@@ -21,4 +26,13 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(TodosContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    delete: todoText => dispatch({type: 'DELETE_TODO', payload: todoText})
+  }
+}
+//here we are making a method that will take in the argument of dispatch which will be a function
+//to return a delete to do action which will let the action be executed anytime dispatch is called
+//flow is mapDispatchToProps => dispatch => action
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
+//this gives the todoscontainer access to this.props.delete because delete was the key 
